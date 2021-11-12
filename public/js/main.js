29b71c1206cd0ba11c97fed8658a7b908aeb0467
lucifer0x17 <ayush.aa347@gmail.com>
2021-11-13 01:52:36 +0530

    wip-oneToOneInteraction

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
 const socket = io();
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+socket.on('socket', (sockets) => {
+    console.log(sockets);
+});
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
-const http = require('http')
-const socketio = require('socket.io')
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,15 +9,22 @@ const io = socketio(server);
 
 const PORT = 3000 || process.env.PORT;
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-let a = 0;
-io.sockets.on('connection', socket => {
-    console.log(socket.id);
-})
+const sockets = {};
 
+io.sockets.on('connection', (socket) => {
+    socket.emit('message', 'Welcome to muj-connect');
+    sockets[socket.id] = socket;
+    console.log(sockets);
+    socket.broadcast.emit('message', 'A user has connected');
 
-server.listen(PORT,() => {
-    console.log(`Listening on ${PORT}`)
+    // console.log(io.engine.clientsCount);
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
 });
