212c6fddcbffa4af3bf37cc142a34657d2084f0a
lucifer0x17 <ayush.aa347@gmail.com>
2021-11-13 04:08:15 +0530

    feat-twoPeopleConnected

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,22 @@
 const socket = io();
 
+const send = document.getElementById('form');
 socket.on('message', (message) => {
     console.log(message);
 });
 
-socket.on('socket', (sockets) => {
-    console.log(sockets);
+socket.on('connect', () => {
+    console.log('Waiting');
+    //Disable the Disconenct/next button
+});
+
+socket.on('connected', () => {
+    //Enable Disconnect/next button
+    console.log('Connected to a user');
+});
+
+send.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const msg = e.target.elements.input.value;
+    socket.emit('chatMessage', msg);
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const moment = require('moment');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,14 +12,37 @@ const PORT = 3000 || process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const sockets = {};
+const sockets = {},
+    users = {};
+
+let strangerQueue = false,
+    peopleActive = 0;
 
 io.sockets.on('connection', (socket) => {
-    socket.emit('message', 'Welcome to muj-connect');
+    // socket.emit('message', 'Welcome to muj-connect');
+    // console.log(moment().format('h:mm a'));
+    // socket.broadcast.emit('message', 'A user has connected');
     sockets[socket.id] = socket;
-    console.log(sockets);
-    socket.broadcast.emit('message', 'A user has connected');
+    users[socket.id] = {
+        connectedTo: -1,
+        isTyping: false,
+    };
 
+    if (strangerQueue !== false) {
+        users[socket.id].connectedTo = strangerQueue;
+        users[socket.id].isTyping = false;
+        users[strangerQueue].connectedTo = socket.id;
+        users[strangerQueue].isTyping = false;
+        socket.emit('connected');
+        console.log('bleh')
+        sockets[strangerQueue].emit('connected');
+        strangerQueue = false;
+    } else {
+        strangerQueue = socket.id;
+    }
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
     // console.log(io.engine.clientsCount);
     socket.on('disconnect', () => {
         io.emit('message', 'User has disconnected');
