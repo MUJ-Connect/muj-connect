254fe71a502ce76d49e65972820cf77b2f6fd55a
lucifer0x17 <ayush.aa347@gmail.com>
2021-11-13 10:49:19 +0530

    feat-twoPeopleConnected

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,13 @@
 const socket = io();
 
-const send = document.getElementById('form');
+const form = document.getElementById('form');
+const button = document.getElementById('button');
 socket.on('message', (message) => {
     console.log(message);
 });
 
 socket.on('connect', () => {
-    console.log('Waiting');
+    console.log('Waiting for a stranger');
     //Disable the Disconenct/next button
 });
 
@@ -15,8 +16,27 @@ socket.on('connected', () => {
     console.log('Connected to a user');
 });
 
-send.addEventListener('submit', (e) => {
+button.addEventListener('click', (e) => {
+    // console.log(e.target.value);
+    if (e.target.innerHTML === 'New Stranger') {
+        //Will be used to find new chat after leaving a chat
+        if (socket) {
+            socket.emit('newStranger');
+            e.target.innerHTML = 'Leave';
+        }
+    } else {
+        //Will be used to leave the chat
+        e.target.innerHTML = 'New Stranger';
+    }
+});
+socket.on('people', ({ people }) => {
+    const component = document.getElementById('people');
+    component.innerHTML = `${people}+ people active`;
+});
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const msg = e.target.elements.input.value;
     socket.emit('chatMessage', msg);
+    // console.log(e.target.elements);
 });
--- server.js
+++ server.js
@@ -18,6 +18,21 @@ const sockets = {},
 let strangerQueue = false,
     peopleActive = 0;
 
+const createConnection = (socket) => {
+    if (strangerQueue !== false) {
+        users[socket.id].connectedTo = strangerQueue;
+        users[socket.id].isTyping = false;
+        users[strangerQueue].connectedTo = socket.id;
+        users[strangerQueue].isTyping = false;
+        socket.emit('connected');
+        // console.log('bleh');
+        sockets[strangerQueue].emit('connected');
+        strangerQueue = false;
+    } else {
+        strangerQueue = socket.id;
+    }
+};
+
 io.sockets.on('connection', (socket) => {
     // socket.emit('message', 'Welcome to muj-connect');
     // console.log(moment().format('h:mm a'));
@@ -28,24 +43,24 @@ io.sockets.on('connection', (socket) => {
         isTyping: false,
     };
 
-    if (strangerQueue !== false) {
-        users[socket.id].connectedTo = strangerQueue;
-        users[socket.id].isTyping = false;
-        users[strangerQueue].connectedTo = socket.id;
-        users[strangerQueue].isTyping = false;
-        socket.emit('connected');
-        console.log('bleh')
-        sockets[strangerQueue].emit('connected');
-        strangerQueue = false;
-    } else {
-        strangerQueue = socket.id;
-    }
+    createConnection(socket);
+    peopleActive++;
+    io.sockets.emit('people', { people: peopleActive });
+    // console.log(`stats: ${peopleActive}`);
+
     socket.on('chatMessage', (msg) => {
         io.emit('message', msg);
+    });
+
+    socket.on('newStranger', () => {
+        createConnection(socket);
     });
     // console.log(io.engine.clientsCount);
     socket.on('disconnect', () => {
         io.emit('message', 'User has disconnected');
+        peopleActive--;
+        // console.log(peopleActive);
+        io.sockets.emit('people', { people: peopleActive });
     });
 });
 
