84f9b6f6f11090ed04af3e0e70519f1a73fd3d51
lucifer0x17 <ayush.aa347@gmail.com>
2021-11-15 23:30:04 +0530

    feat:completed

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,10 @@ const socket = io();
 
 const form = document.getElementById('form');
 const button = document.getElementById('button');
-socket.on('message', (message) => {
-    console.log(message);
+let isTyping = false;
+
+socket.on('text', (message) => {
+    console.log(`Stranger:${message}`);
 });
 
 socket.on('connect', () => {
@@ -14,29 +16,75 @@ socket.on('connect', () => {
 socket.on('connected', () => {
     //Enable Disconnect/next button
     console.log('Connected to a user');
+    button.innerHTML = 'Leave';
 });
 
 button.addEventListener('click', (e) => {
     // console.log(e.target.value);
-    if (e.target.innerHTML === 'New Stranger') {
+    if (e.target.innerHTML === 'New Stranger' && socket) {
         //Will be used to find new chat after leaving a chat
-        if (socket) {
-            socket.emit('newStranger');
-            e.target.innerHTML = 'Leave';
-        }
-    } else {
+        socket.emit('newStranger');
+        // console.log('bleh');
+    } else if (e.target.innerHTML === 'Leave' && socket) {
         //Will be used to leave the chat
-        e.target.innerHTML = 'New Stranger';
+        socket.emit('leave');
     }
 });
+
 socket.on('people', ({ people }) => {
     const component = document.getElementById('people');
     component.innerHTML = `${people}+ people active`;
 });
 
+socket.on('typing', (typing) => {
+    if (typing) {
+        console.log('stranger typing');
+    } else {
+        console.log('stopped');
+    }
+});
+
+socket.on('leave', ({ user, error }) => {
+    button.innerHTML = 'New Stranger';
+    console.log(`${user} disconnected!`);
+    if (error) {
+        console.log(`reason: ${error}`);
+    }
+});
+
+const sendText = (msg) => {
+    if (msg.length > 0) {
+        if (isTyping) {
+            socket.emit('typing', false);
+        }
+        isTyping = false;
+        socket.emit('text', msg);
+        console.log(`Me:${msg}`);
+    }
+};
+
+form.addEventListener('keyup', (e) => {
+    const keyCode = e.key;
+    if (keyCode !== 'Enter') {
+        // console.log(keyCode);
+        // console.log(e.path[0].value);
+        if (socket) {
+            if (e.path[0].value === '' && isTyping) {
+                socket.emit('typing', false);
+                isTyping = false;
+            } else if (!isTyping && e.path[0].value.length > 0) {
+                socket.emit('typing', true);
+                isTyping = true;
+            }
+        }
+    }
+});
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const msg = e.target.elements.input.value;
-    socket.emit('chatMessage', msg);
-    // console.log(e.target.elements);
+    const elem = e.target.elements.input;
+    sendText(elem.value);
+    elem.value = '';
+    elem.focus();
+    // console.log(msg);
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const moment = require('moment');
+const { count } = require('console');
 
 const app = express();
 const server = http.createServer(app);
@@ -19,7 +20,11 @@ let strangerQueue = false,
     peopleActive = 0;
 
 const createConnection = (socket) => {
-    if (strangerQueue !== false) {
+    // if (peopleActive === 1) {
+    //     console.log('No user to connect');
+    //     return;
+    // }
+    if (strangerQueue !== false && peopleActive > 1) {
         users[socket.id].connectedTo = strangerQueue;
         users[socket.id].isTyping = false;
         users[strangerQueue].connectedTo = socket.id;
@@ -43,23 +48,69 @@ io.sockets.on('connection', (socket) => {
         isTyping: false,
     };
 
-    createConnection(socket);
     peopleActive++;
+    createConnection(socket);
     io.sockets.emit('people', { people: peopleActive });
     // console.log(`stats: ${peopleActive}`);
-
-    socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
-    });
 
     socket.on('newStranger', () => {
         createConnection(socket);
     });
+
+    socket.on('text', (msg) => {
+        if (
+            users[socket.id].connectedTo !== -1 &&
+            sockets[users[socket.id].connectedTo]
+        ) {
+            sockets[users[socket.id].connectedTo].emit('text', msg);
+        }
+    });
+
+    socket.on('typing', (isTyping) => {
+        if (
+            users[socket.id].connectedTo !== -1 &&
+            sockets[users[socket.id].connectedTo] &&
+            users[socket.id].isTyping !== isTyping
+        ) {
+            users[socket.id].isTyping = isTyping;
+            sockets[users[socket.id].connectedTo].emit('typing', isTyping);
+        }
+    });
+
+    socket.on('leave', () => {
+        let stranger = users[socket.id].connectedTo;
+        if (strangerQueue === socket.id || strangerQueue === stranger) {
+            strangerQueue = false;
+        }
+        users[socket.id].connectedTo = -1;
+        users[socket.id].isTyping = false;
+        if (sockets[stranger]) {
+            users[stranger].connectedTo = -1;
+            users[stranger].isTyping = false;
+            sockets[stranger].emit('leave', { user: 'stranger' });
+        }
+        socket.emit('leave', { user: 'you' });
+    });
+
     // console.log(io.engine.clientsCount);
-    socket.on('disconnect', () => {
-        io.emit('message', 'User has disconnected');
-        peopleActive--;
+    socket.on('disconnect', (error) => {
         // console.log(peopleActive);
+        let stranger = users[socket.id] && users[socket.id].connectedTo;
+        if (stranger === undefined) stranger = -1;
+        if (stranger !== -1 && sockets[stranger]) {
+            sockets[stranger].emit('leave', {
+                user: 'stranger',
+                error: error && error.toString(),
+            });
+            users[stranger].connectedTo = -1;
+            users[stranger].isTyping = false;
+        }
+        delete sockets[socket.id];
+        delete users[socket.id];
+        if (strangerQueue === socket.id || strangerQueue === stranger) {
+            strangerQueue = false;
+        }
+        peopleActive--;
         io.sockets.emit('people', { people: peopleActive });
     });
 });
